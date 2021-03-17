c68fd8ca93bdf5d9b0f291a77f5b316da19c620d
Gabriel Aspir <gaspir@mail.yu.edu>
2021-03-16 22:58:33 -0400

    Finished!
    Thanks Youtube!

--- hangman.py
+++ hangman.py
@@ -16,21 +16,41 @@ def hangman():
     wordLetters = set(word) # letters in the word
     alphabet = set(string.ascii_uppercase)
     usedLetters = set()
+    lives = 8
 
     #Now we're gonna ask the user for a letter to guess
-    guessedLetter = input('Guess a letter:').upper()
-    if (guessedLetter in alphabet) and (guessedLetter not in usedLetters) :
-        usedLetters.add(guessedLetter)
-        #Was it a correctly guessed letter contained in our wordLetters set or not?
-        if guessedLetter in wordLetters:
-            wordLetters.remove(guessedLetter)
+    while len(wordLetters) > 0 and lives > 0: #make them keep guessing
+        print('You have', lives,'lives left\n' 'Used Letters: ', ' '.join(usedLetters)) #' '.join() puts a space inbetween all of the used letters and concatenates them into 1 string
+        print()
+        #Next print out what the current word is
+        # put whatever letter, if it is in usedLetters, or put a - for each letter in word
+        wordList = [letter if letter in usedLetters else '-' for letter in word]
+        print('Word is', ' '.join(wordList))
+        print()
 
-    elif guessedLetter in usedLetters:
-        print('You already guessed that letter! Try again')
 
-    #Invalid input
+
+        guessedLetter = input('Guess a letter:').upper()
+        print()
+        if (guessedLetter in alphabet) and (guessedLetter not in usedLetters) :
+            usedLetters.add(guessedLetter)
+            #Was it a correctly guessed letter contained in our wordLetters set or not?
+            if guessedLetter in wordLetters:
+                wordLetters.remove(guessedLetter)
+            else:
+                lives = lives - 1
+                print('Letter isn\'t in word')
+        elif guessedLetter in usedLetters:
+            print('You already guessed that letter! Try again')
+
+        #Invalid input
+        else:
+            print('Not a letter in the alphabet, c\'mon')
+
+
+    if lives == 0:
+        print('Loser, sorry! The word was', word)
     else:
-        print('Not a letter in the alphabet, c\'mon')
+        print('You got it! The word was', word, '!')
 
-userInput = input('Type something:')
-print(userInput)
+hangman()
